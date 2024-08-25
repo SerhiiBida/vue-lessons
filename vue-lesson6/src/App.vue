07aa24b1da35aa467ef663bb9f26f3c95cf3a67d
Sergii Bida <sergijbida15@gmail.com>
2024-08-25 15:11:38 +0300

    vue-lesson6

---
+++ vue-lesson6/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: "App",
+}
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink :to="{name: 'home'}">
+        Home
+      </RouterLink>
+    </nav>
+  </header>
+
+  <main>
+    <RouterView/>
+  </main>
+
+  <footer>
+  </footer>
+</template>
