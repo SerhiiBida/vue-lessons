c2e8a6cd6f58eda18e4b2c27c36bfe2b6597511a
Sergii Bida <sergijbida15@gmail.com>
2024-08-26 17:25:25 +0300

    did 9 task

--- vue-lesson7/src/components/AddText.vue
+++ vue-lesson7/src/components/AddText.vue
@@ -1,9 +1,9 @@
 <script>
-import {formMixin} from "@/mixins/form.js";
+import {formInputTextMixin} from "@/mixins/form.js";
 
 export default {
   name: "AddText",
-  mixins: [formMixin],
+  mixins: [formInputTextMixin],
   props: {
     modelValue: String,
   }
@@ -12,17 +12,23 @@ export default {
 
 <template>
   <section>
-    <form action="#">
-      <input
-          type="text"
-          name="text"
-          id="text"
-          :placeholder="inputText.placeholder"
-          :maxlength="inputText.maxLength"
-      >
-      <button type="submit">
-        Add
-      </button>
+    <form action="#" @submit.prevent="updateText">
+      <div>
+        <input
+            v-model="inputText.text"
+            type="text"
+            name="text"
+            id="text"
+            :placeholder="inputText.placeholder"
+            :maxlength="inputText.maxLength"
+        >
+        <button type="submit">
+          Add
+        </button>
+      </div>
+      <p :style="inputText.errorStyles">
+        {{ inputText.error }}
+      </p>
     </form>
   </section>
 </template>
--- vue-lesson7/src/mixins/form.js
+++ vue-lesson7/src/mixins/form.js
@@ -1,9 +1,41 @@
-export const formMixin = {
+// 6. Створіть базовий міксін, який додає декілька методів або даних у компонент.
+// 9. Розробіть міксін, який додає валідацію для форми в компоненті.
+export const formInputTextMixin = {
     data() {
         return {
             inputText: {
                 placeholder: "Enter text...",
-                maxLength: 40
+                maxLength: 40,
+                text: "",
+                errorStyles: {
+                    color: "red"
+                },
+                error: ""
+            }
+        }
+    },
+    methods: {
+        clearErrors() {
+            this.inputText.error = "";
+        },
+        textValidation() {
+            this.inputText.text = this.inputText.text.trim();
+
+            if (!this.inputText.text) {
+                this.inputText.error = "Поле не може бути пустим";
+
+                return false;
+            }
+
+            return true;
+        },
+        updateText() {
+            this.clearErrors();
+
+            if (this.textValidation()) {
+                this.$emit("update:modelValue", this.inputText.text);
+
+                this.inputText.text = "";
             }
         }
     }
